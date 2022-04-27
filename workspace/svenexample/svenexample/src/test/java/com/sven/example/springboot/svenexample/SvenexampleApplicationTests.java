a549184488c3865a423a606b2032e3dfbb992a51
Sven Karmann <svenkarmann@gmx.de>
2022-04-27 17:16:41 +0200

    insert example ws

---
+++ workspace/svenexample/svenexample/src/test/java/com/sven/example/springboot/svenexample/SvenexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sven.example.springboot.svenexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvenexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
