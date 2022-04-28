e698a24c62f9785348c055440b8fac64cdb4b3ff
Sven Karmann <svenkarmann@gmx.de>
2022-04-28 11:30:41 +0200

    dev test

--- udemy_workspace/svenexample/src/main/java/com/sven/example/springboot/svenexample/SvenexampleApplication.java
+++ udemy_workspace/svenexample/src/main/java/com/sven/example/springboot/svenexample/SvenexampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SvenexampleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! This is a git Test");
 		SpringApplication.run(SvenexampleApplication.class, args);
 	}
 
